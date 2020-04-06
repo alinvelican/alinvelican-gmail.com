4ddfa91f622d52b3d84cdfbdcb3b0f3760495122
alinvelican <alinvelican@gmail.com>
2020-04-06 14:41:32 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-srv"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/cloudrun/hello"
+      }
+    }
+  }
+}
