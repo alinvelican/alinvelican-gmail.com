ce4b1cffa69bf8bfb2678579df60011a9f6f033b
alinvelican <alinvelican@gmail.com>
2020-04-06 15:19:55 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_cloud_run_service" "default" {
-  name     = "cloudrun-srv"
+  name     = "cloudrun-srv2"
   location = "us-central1"
   project  = "temporal-ground-251311"
 
@@ -11,3 +11,19 @@ resource "google_cloud_run_service" "default" {
     }
   }
 }
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+  }
